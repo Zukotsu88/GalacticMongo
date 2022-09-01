260af0d74fea18749c3f553e44a4381259b4481b
Amir Razvi <zukotsu88@gmail.com>
2022-09-01 00:46:55 -0500

    Add project files.

---
+++ GalacticMongo/Models/Galaxy.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GalacticMongo.Models
+{
+    // the model class for the galaxy
+    public class Galaxy
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("name")]
+        public string? Name { get; set; }
+
+        [BsonElement("type")]
+        public string? Type { get; set; }
+
+        [BsonElement("diameter")]
+        public int Diameter { get; set; }
+
+        [BsonElement("space_unit")]
+        public string? SpaceUnit { get; set; }
+
+        [BsonElement("black_hole")]
+        public string? BlackHole { get; set; }
+
+        [BsonElement("age")]
+        public double Age { get; set; }
+
+        [BsonElement("age_units")]
+        public string? AgeUnits { get; set; }
+    }
+}
---
+++ GalacticMongo/Repositories/GalacticRepository.cs
@@ -0,0 +1,37 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace GalacticMongo.Repositories
+{
+    public class GalacticRepository
+    {
+
+        /*
+                 private readonly IMongoCollection<Comment> _commentsCollection;
+                 private readonly MoviesRepository _moviesRepository;
+
+        public CommentsRepository(IMongoClient mongoClient)
+        {
+            var camelCaseConvention = new ConventionPack {new CamelCaseElementNameConvention()};
+            ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+            _commentsCollection = mongoClient.GetDatabase("sample_mflix").GetCollection<Comment>("comments");
+            _moviesRepository = new MoviesRepository(mongoClient);
+        }*/
+ 
+        private MongoClient IMongoCollection<Galaxy> _galaxyCollection;
+        
+        public GalacticRepository(IMongoClient mongoClient)
+        {
+            _galaxyCollection = mongoClient.GetDatabase("galaxies").GetCollection<Galaxy>("galacticCol");
+        }
+
+
+    }
+}
